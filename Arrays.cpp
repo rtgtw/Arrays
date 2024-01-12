1b9ff2288a96369fcbc33f440d2ab026058febdc
Remi Olateru <remiolateru@gmail.com>
2024-01-12 05:16:30 -0500

    Create delete logic

--- Arrays.cpp
+++ Arrays.cpp
@@ -13,7 +13,8 @@ struct Array {
 	int length;
 };
 
-
+//Displays the elements within an array
+//Displays only from 0 to length, not 0 to the full size 
 void display(struct Array arr) {
 	int i;
 	std::cout << "Elements are: " << '\n';
@@ -22,17 +23,33 @@ void display(struct Array arr) {
 	}
 };
 
+
+
 //Inserts a new element at the end of the array
 //we will use a pointer as the parameter since we want to change the value by address
-
 void append(struct Array *arr, int x) {
 
+
+	// This verifies that there is room inside of the array
+	//if length = size then there is no room inside of the array
+	//Meaning we cannot perform the append logic
 	if (arr->length < arr->size) {
 		arr->A[arr->length++] = x;
 		
 	}
 };
+
+
+
+
+
+
+
+
+
 //Take the array by ADDRESS since it will modify the array
+//This is pointing to the OBJECT arr, not a particular arr, just the WHOLE object
+//Meaning we can modify its member variables 
 void insert(struct Array *arr, int index, int value) {
 
 	//First we have to check if the index is valid, which is 0 to the length of the array
@@ -41,11 +58,14 @@ void insert(struct Array *arr, int index, int value) {
 
 
 	if (index >= 0 && index <= arr->length) {
+
+		//Creates a for loop that iterates from the length of the array
+		//down to the value of index(where the user wants the value to go)
 		for (int i = arr->length; i > index; i--) {
 
 			//A[i] represents the next available location
 			//we want to take the last element inside of the array with i-1, and equal it to A[i]
-			//this will shift all of the elements up to index, 
+			//this will shift all of the elements up until we hit  index(location of newly inserted value), 
 			arr->A[i] = arr->A[i - 1];
 		};
 		//then we insert our values since everything has been shifted
@@ -59,19 +79,77 @@ void insert(struct Array *arr, int index, int value) {
 };
 
 
+
+//Delete an element within an array
+//We use int here in order to return a success or failure
+//Success will return the element deleted, failure will return 1
+//This function requires a pointer for this ADT Array, and the index to be deleted
+int deleteElement(struct Array *arr, int index) {
+	
+	//Deletion value, initialized
+	int deletedValue = 0;
+
+
+	//Check to see if the index from the input is valid, if it is between
+	//0 and the length - 1, since length vacant
+	if (index >= 0 && index <= arr->length - 1) {
+
+		//this represents the actual value that we will delete after the for loop
+		//this value will get printed back to the user
+		deletedValue = arr->A[index];
+
+
+	//This for loop starts off at the position of index, which is where the user wants to
+	//make a deletion, the reason why its up to length - 1 is because:
+	//there is no value at length, the last value inside of our array is length - 1
+	//So if we go from userIndex < length, then our logic will copy the content
+	// of length  and place it into length -1, which is incorrect, we are only worried about
+	// the elements within our array, and length is not apart of our array
+		for (int i = index; i < arr->length - 1; i++) {
+
+			//transfers the content of index + 1, into index, which shifts everything to the left
+			arr->A[i] = arr->A[i + 1];
+		}
+
+		//Decrease the length by 1 since we removed a value
+		arr->length--;
+
+		//return the deleted value back to user
+		return deletedValue;
+	};
+
+	//if the user enters an invalid index, return 1 instead to signify an error
+	return 1;
+};
+
+
+
+
+
+
+
+
+
+
+
 int main(){
 
 	
-	struct Array arr = { {2,3,4,6,8}, 10,5 };
+	struct Array arr = { {22,44,55,66,77}, 10,5 };
 
 	int i;
 	int v;
-	std::cin >> i;
-	std::cin >> v;
+	//std::cin >> i;
+	//std::cin >> v;
 
-	insert(&arr, i, v);
-	insert(&arr, 6, 525);
+	//insert(&arr, i, v);
+	//insert(&arr, 5, 525);
 	append(&arr, 99);
+	//std::cout << deleteElement(&arr, 3) << '\n' << '\n';
+	append(&arr, 44);
+	//std::cout << deleteElement(&arr, 2) << '\n' << '\n';
+	append(&arr, 62);
+	//std::cout << deleteElement(&arr, arr.length - 1) << '\n' << '\n';
 	display(arr);
 
 
